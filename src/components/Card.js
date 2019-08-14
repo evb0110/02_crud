cd15cac5020cf57c835abc397bd13d30cbb5af59
Eugene Barsky <34789976+evb0110@users.noreply.github.com>
2019-08-14 21:26:45 +0300

    process

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Card({post}) {
+  return (
+    <div className="card">
+      <div className="close">
+      <span className="close-button">X</span>
+      </div>
+      <img className="avatar" src="https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png" alt="avatar"></img>
+      <div className="author">Author: Lorem</div>
+      <div>Created: {post.created} ago</div>
+      <h1>{post.content}</h1>
+    </div>
+  );
+}
