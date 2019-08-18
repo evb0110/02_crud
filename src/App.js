d476bc9651192f174d9fd801c50e5f50444cce4c
evb0110 <7149553@gmail.com>
2019-08-18 20:21:37 +0300

    process stuck

--- src/App.js
+++ src/App.js
@@ -1,27 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import axios from 'axios';
 
 import Card from './components/Card';
+import Create from './components/Create';
+import CardEdit from './components/CardEdit';
 
-const data = [
-  {
-    id: 1,
-    content: 'Какая-то фигня',
-    created: '5 мин',
-  },
-  {
-    id: 2,
-    content: 'Очень важный текст',
-    created: '10 мин',
-  },
-];
+// const data = [
+//   {
+//     id: 1,
+//     content: 'Какая-то фигня',
+//     created: '5 мин',
+//   },
+//   {
+//     id: 2,
+//     content: 'Очень важный текст',
+//     created: '10 мин',
+//   },
+// ];
 
 function App() {
+  const postsUrl = 'http://localhost:7777/posts';
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get(postsUrl).then(response => setData(response.data));
+  });
+
   return (
     <div className="App">
-      {data.map(post => (
-        <Card post={post} />
-      ))}
+      <Router>
+        <Link to="/posts">
+          <button className="button">Домашняя страница</button>
+        </Link>
+        <Link to="/posts/new">
+          <button className="button">Создать пост</button>
+        </Link>
+        <Switch>
+        <Route path="/posts/new" exact component={Create} />
+        <Route path="/posts/:id" exact component={CardEdit} />
+          <Route
+            path="/posts"
+            exact
+            component={() => data.map(post => <Card post={post} />)}
+          />
+        </Switch>
+      </Router>
     </div>
   );
 }
